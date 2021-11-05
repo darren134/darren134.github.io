2c1872a7b17f3d66c7ae269b0852292a1fbdaa8b
darren134 <85605515+darren134@users.noreply.github.com>
2021-11-05 08:55:39 +0700

    Css & js upload

---
+++ page.js
@@ -0,0 +1,12 @@
+var div = document.getElementById("toggle");
+var sp1 = document.getElementById("sp1");
+var sp2 = document.getElementById("sp2");
+var sp3 = document.getElementById("sp3");
+const nih = document.querySelector('nav ul');
+
+function lah() {
+			sp1.classList.toggle("an1");
+			sp2.classList.toggle("an2");
+			sp3.classList.toggle("an3");
+			nih.classList.toggle("cih");
+};
